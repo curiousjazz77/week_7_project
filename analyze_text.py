0a458b0d4032331f9825ac9e7536adb7acf94b38
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2020-07-19 20:35:40 -0700

    install dependencies using Pipfile; continue project

--- analyze_text.py
+++ analyze_text.py
@@ -1,8 +1,9 @@
-from collections import Counter
+from collections import Counter, defaultdict
 from heapq import *
 import re
 import unicodedata
 import urllib.request
+import nltk.data
 
 
 class GutenbergAnalyzer:
@@ -11,23 +12,37 @@ class GutenbergAnalyzer:
         self.url = url
         self.total_words = []
         self.unique_words = set()
+        self.common_words_url = "https://gist.githubusercontent.com/deekayen/4148741/raw/98d35708fa344717d8eee15d11987de6c8e26d7d/1-1000.txt"
+        self.common_words = []
+        self.interesting_word_mode = False
+        self.chapter_progression_words = defaultdict(lambda: defaultdict(int))
 
     def remove_control_characters(self, s):
-        return "".join(ch for ch in s if unicodedata.category(ch)[0]!="C")
-
+        return "".join(ch for ch in s if unicodedata.category(ch)[0] != "C")
 
     def get_words_per_line(self, line):
-        words_only = re.sub(r'\W+', ' ', line)
+        # words_only = re.sub(r'\W+', ' ', line)
+        # words_only = re.sub(r"(\w+'\w+)", " ", line)
+        words_only = re.sub(r"(^|\W)\d+", " ", line)
         control_characters_removed = self.remove_control_characters(words_only)
-        sanitized_line = control_characters_removed.strip().split()
+        sanitized_line = (
+            control_characters_removed.strip()
+            .replace(".", "")
+            .replace('"', "")
+            .replace('\'', "")
+            .replace(",", "")
+            .replace("-", "")
+            .replace("_", "")
+            .replace("!", "")
+            .split()
+        )
+        sanitized_line = [x.lower() for x in sanitized_line]
         return sanitized_line
-
 
     def getTotalUniqueWords(self):
         if len(self.total_words) == 0:
             self.parse_file_for_all_words()
         return len(set(self.total_words))
-
 
     def parse_file(self):
         data_file = urllib.request.urlopen(self.url)
@@ -35,17 +50,15 @@ class GutenbergAnalyzer:
             decoded_line = line.decode("utf-8")
             yield decoded_line
 
-
     def check_if_viable_line(self, line):
         if "THE PREFACE" in line:
             self.viable_word_line = True
+            self.chapter_progression_words["PREFACE"] # add first
         if "End of Project" in line:
             self.viable_word_line = False
         return
 
-
     def parse_file_for_all_words(self):
-        viable_word_line = False
         for line in self.parse_file():
             self.check_if_viable_line(line)
             if self.viable_word_line:
@@ -65,12 +78,80 @@ class GutenbergAnalyzer:
 
         heap = []
         for word, frequency in c.items():
-            heappush(h, [-frequency, word])
+            heappush(heap, [-frequency, word])
         res = []
         while k > 0:
-            f, w = heappop(h)
-            res.append([w, f])
+            f, wd = heappop(heap)
+            if self.interesting_word_mode:
+                if wd not in self.common_words:
+                    res.append([wd, -f])
+                    k -= 1
+                else:
+                    continue
+            else:
+                res.append([wd, -f])
+                k -= 1
         return res
+
+    def get20LeastFrequentWords(self):
+        k = 20
+        if len(self.total_words) == 0:
+            self.parse_file_for_all_words()
+        c = Counter(self.total_words)
+
+        heap = []
+        for word, frequency in c.items():
+            heappush(heap, [frequency, word])
+        res = []
+        while k > 0:
+            f, wd = heappop(heap)
+            if self.interesting_word_mode:
+                if wd not in self.common_words:
+                    res.append([wd, f])
+                    k -= 1
+                else:
+                    continue
+            else:
+                res.append([wd, f])
+                k -= 1
+        return res
+
+    def parse_most_common_words(self):
+        data_file = urllib.request.urlopen(self.common_words_url)
+        for line in data_file:
+            decoded_line = line.decode("utf-8")
+            sanitized_line = self.get_words_per_line(decoded_line)
+            sanitized_line = [x.lower() for x in sanitized_line]
+            self.common_words.extend(sanitized_line)
+        self.common_words = set(self.common_words)
+
+    def get20MostInterestingFrequentWords(self):
+        self.parse_most_common_words()
+        self.interesting_word_mode = True
+        return self.get20MostFrequentWords()
+
+    def getFrequencyOfWord(self, word): # per chapter
+        current_key = "PREFACE"
+        for line in self.parse_file():
+            self.check_if_viable_line(line)
+            if self.viable_word_line:
+                if "CHAPTER" in line:
+                    line = self.remove_control_characters(line)
+                    current_key = line
+                words = self.get_words_per_line(line)
+                for w in words:
+                    self.chapter_progression_words[current_key][w] += 1
+
+            else:
+                continue
+        res = []
+        for k, v in self.chapter_progression_words.items():
+            if w in v:
+                res.append( [k, self.chapter_progression_words[k][w]])
+
+        return res
+
+    def getChapterQuoteAppears(self, quote):
 
 
 
@@ -79,10 +160,39 @@ def main():
     url = "https://raw.githubusercontent.com/GITenberg/The-Picture-of-Dorian-Gray_174/master/174.txt"
     g = GutenbergAnalyzer(url)
     total_words = g.getTotalNumberOfWords()
-    print('Total words in novel', total_words)
+    print("Total words in novel", total_words)
     unique_words = g.getTotalUniqueWords()
-    print('Number of unique words in novel', unique_words)
+    print("Number of unique words in novel", unique_words)
 
+    top_20 = g.get20MostFrequentWords()
+    print("Top 20 words")
+    print(*top_20, sep="\n")
+    print("\n")
+
+    top_20_interesting = g.get20MostInterestingFrequentWords()
+    print("Top 20 interesting words")
+    print(*top_20_interesting, sep="\n")
+    print("\n")
+
+    bottom_20 = g.get20LeastFrequentWords()
+    print("Top 20 words")
+    print(*bottom_20, sep="\n")
+    print("\n")
+
+    word = "dorian"
+    frequency_by_chapter = g.getFrequencyOfWord(word)
+    print("Frequency of word: [", word, "] per chapter")
+    print(*frequency_by_chapter, sep="\n")
+    print("\n")
+
+    word = "the"
+    frequency_by_chapter = g.getFrequencyOfWord(word)
+    print("Frequency of word:[", word, "] per chapter")
+    print(*frequency_by_chapter, sep="\n")
+    print("\n")
+
+
+    quote = "Yes, Dorian, you will always be fond of me"
 
 
 if __name__ == "__main__":
